88e28bcd970749c0242e498e0aa53e4ef3ea33f4
Code-Nature <codenature01001@gmail.com>
2022-03-21 21:07:57 -0500

    publico mi portafolio en github pages version 1

---
+++ src/components/about/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import './about.css';
+import ME from '../../assets/me-about.png';
+import {FiAward} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+
+const About = () => {
+    return (
+        <section id="about">
+             <h5>Get To Know</h5>
+             <h2>About Me</h2>
+
+             <div className="container about__container">
+                 <div className="about__me">
+                     <div className="about__me-image">
+                         <img src={ME} alt="" />
+                     </div>
+                 </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FiAward className="about-icon"/>
+                            <h5>Experience</h5>
+                            <small>3+ Years Working</small>
+                        </article>
+
+                        <article className="about__card">
+                            <VscFolderLibrary className="about-icon"/>
+                            <h5>Projects</h5>
+                            <small>80+ Completed</small>
+                        </article>
+
+                    </div>
+                        <p>
+                        ¡Hola! Mi nombre es Camilo y disfruto creando cosas que viven en internet. Mi interés en el desarrollo web comenzó en 2019 cuando en el colegio a final de grado nos ensellaron algo de lo que tenia que ver con operaciones en javaScript, desde entonces empezo mi interez por la tecnologia web.
+                        </p>
+
+                        <a href="#contact" className="btn btn-primary">Let's Talk</a>
+                </div>
+
+            </div> 
+        </section>
+    )
+}
+
+export default About
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,65 @@
+import React from "react"
+import './contact.css'
+import { HiOutlineMail } from "react-icons/hi"
+import { RiMessengerLine } from "react-icons/ri"
+import { FaWhatsapp } from "react-icons/fa"
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_oaoobcq', 'template_7kfn0sg', form.current, '18L-HqxP-qP0gwWPn')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        < HiOutlineMail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5>random@gmail.com</h5>
+                        <a href="mailto:buitragoivancamilo1025@gmail.com" target="_blank"> Send a Message </a>
+                    </article>
+
+                    <article className="contact__option">
+                        < RiMessengerLine className="contact__option-icon" />
+                        <h4>Messenger</h4>
+                        <h5>Code_NNature</h5>
+                        <a href="https://m.me/ivan.buitrago.988/" target="_blank"> Send a Message </a>
+                    </article>
+
+                    <article className="contact__option">
+                        < FaWhatsapp className="contact__option-icon" />
+                        <h4>WhatsApp</h4>
+                        <h5>329674652</h5>
+                        <a href="https://api.whatsapp.com/send?phone=+573204833432" target="_blank"> Send a Message </a>
+                    </article>
+                </div>
+
+                {/* END OF CONTACT OPTIONS */}
+
+                <form ref={form} onSubmit={sendEmail} autoComplete="off">
+                    <input type="text" name="name" placeholder="Your Full Name" required id="" />
+                    <input type="email" name="email" placeholder="Your Email" required id="" />
+                    <textarea name="message" id="" cols="" rows="7" placeholder="Your Message" required></textarea>
+                    <button type="submit" className="btn btn-primary">Send Message</button>
+                </form>
+            </div>
+        </section >
+    )
+}
+
+export default Contact
