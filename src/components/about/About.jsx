435a1b1927c0ab7fe4bb1d48e02daf4421629e88
Code-Nature <codenature01001@gmail.com>
2023-01-26 21:07:03 -0500

    se agrego informacion hacerca de mi

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,42 +6,42 @@ import { VscFolderLibrary } from 'react-icons/vsc'
 
 
 const About = () => {
-    return (
-        <section id="about">
-            <h2>Hacerca de mi</h2>
+  return (
+    <section id="about">
+      <h2>Hacerca de mi</h2>
 
-            <div className="container about__container">
-                <div className="about__me">
-                    <div className="about__me-image">
-                        <img src={ME} alt="" />
-                    </div>
-                </div>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
 
-                <div className="about__content">
-                    <div className="about__cards">
-                        <article className="about__card">
-                            <FiAward className="about-icon" />
-                            <h5>Experiencia</h5>
-                            <small>3+ Años trabajando</small>
-                        </article>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className="about-icon" />
+              <h5>Experiencia</h5>
+              <small>3+ Años trabajando</small>
+            </article>
 
-                        <article className="about__card">
-                            <VscFolderLibrary className="about-icon" />
-                            <h5>Proyectos</h5>
-                            <small>20+ Completados</small>
-                        </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about-icon" />
+              <h5>Proyectos</h5>
+              <small>20+ Completados</small>
+            </article>
 
-                    </div>
-                    <p>
-                        ¡Hola! Mi nombre es Camilo y disfruto creando cosas que viven en internet. Mi interés en el desarrollo web comenzó en 2019 cuando en el colegio a final de grado nos ensellaron algo de lo que tenia que ver con operaciones en javaScript, desde entonces empezo mi interez por la tecnologia web.
-                    </p>
+          </div>
+          <p>
+          Mi nombre es Camilo y soy un desarrollador web con experiencia en la creación y implementación de proyectos en línea. Mi interés en el campo comenzó en 2019, cuando tuve la oportunidad de aprender sobre el lenguaje de programación JavaScript en mi último año de educación secundaria. Desde entonces, he continuado desarrollando mis habilidades y conocimientos en el campo del desarrollo web, buscando siempre mejorar y ampliar mi experiencia en el campo.
+          </p>
 
-                    <a href="#contact" className="btn btn-primary">Hablemos</a>
-                </div>
+          <a href="#contact" className="btn btn-primary">Hablemos</a>
+        </div>
 
-            </div>
-        </section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default About
